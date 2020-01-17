1cdeaabe5f656ada70e7d8ae67698aa605d27686
Howmuchadollarcost <muluyonas@yahoo.com>
2020-01-17 12:27:26 +0100

    Updates

---
+++ precache-manifest.8f39a0c5eabd9f8826eb8d8507f3f1f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d1e7f664b4ff45ce49b4b323994258",
+    "url": "/church-app/index.html"
+  },
+  {
+    "revision": "7fcf5e2bf8ec8799d0aa",
+    "url": "/church-app/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "453dede76e57194b0edb",
+    "url": "/church-app/static/css/main.f600e04f.chunk.css"
+  },
+  {
+    "revision": "7fcf5e2bf8ec8799d0aa",
+    "url": "/church-app/static/js/2.7a8eb2d1.chunk.js"
+  },
+  {
+    "revision": "453dede76e57194b0edb",
+    "url": "/church-app/static/js/main.9bf03514.chunk.js"
+  },
+  {
+    "revision": "6e946feaa9013117416f",
+    "url": "/church-app/static/js/runtime~main.14500cb0.js"
+  },
+  {
+    "revision": "d86441ea02a000afdafd5b0e978e10ce",
+    "url": "/church-app/static/media/Urael.d86441ea.jpg"
+  },
+  {
+    "revision": "9adb248e82b6878ade2a200d26f3d8d0",
+    "url": "/church-app/static/media/logo1.9adb248e.png"
+  }
+]);
